ea7357f5a8bf793935a9bdc60e44e0b412be1105
Thomas Dunn <73040642+thomasdunn15@users.noreply.github.com>
2024-12-26 17:41:35 -0500

    Add files via upload

---
+++ Assignment1/Assignment1.java
@@ -0,0 +1,17 @@
+/**
+ * Last Name, First Name 
+ * COP-3252
+ * Assignment 1
+ * Date
+ */
+public class Assignment1
+{
+  /**
+   * My main method
+   * @param args array of String arguments that may be included in the command-line arguments
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome to COP-3252 Java Programming!");
+  }
+}
